dd9117f7336f73e3287dafb7f85bdba59eef10ec
Kristen Lavavej <35638702+klavavej@users.noreply.github.com>
2023-06-08 07:15:49 -0700

    Update environment.ts

--- netlify/edge-functions/environment.ts
+++ netlify/edge-functions/environment.ts
@@ -1,9 +1,9 @@
 import type { Context } from "https://edge.netlify.com";
 
 export default async (request: Request, context: Context) => {
-  const value = Deno.env.get("MY_IMPORTANT_VARIABLE");
+  const value = Netlify.env.has("MY_IMPORTANT_VARIABLE");
 
-  return new Response(`Value of MY_IMPORTANT_VARIABLE is "${value}".`, {
+  return new Response(`Variable MY_IMPORTANT_VARIABLE exists? "${value}".`, {
     headers: { "content-type": "text/html" },
   });
 };

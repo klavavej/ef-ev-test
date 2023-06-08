edee501b227f1d56bc12aeaec13ca1f16a9b8bf9
Kristen Lavavej <35638702+klavavej@users.noreply.github.com>
2023-06-08 07:19:06 -0700

    Update environment.ts

--- netlify/edge-functions/environment.ts
+++ netlify/edge-functions/environment.ts
@@ -2,8 +2,9 @@ import type { Context } from "https://edge.netlify.com";
 
 export default async (request: Request, context: Context) => {
   const value = Netlify.env.has("MY_IMPORTANT_VARIABLE");
+  const value2 = Netlify.env.has("MY_FAKE_VARIABLE");
 
-  return new Response(`Variable MY_IMPORTANT_VARIABLE exists? "${value}".`, {
+  return new Response(`Variable MY_IMPORTANT_VARIABLE exists? "${value}". ariable MY_FAKE_VARIABLE exists? "${value2}".`, {
     headers: { "content-type": "text/html" },
   });
 };
